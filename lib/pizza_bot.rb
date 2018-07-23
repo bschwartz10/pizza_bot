9503a9175458eec58f22e5403d75ca950918d48d
bschwartz10 <bschwartz92@gmail.com>
2018-07-23 16:32:28 -0400

    Refactor multiple methods
    remove width, height, max_dimensions from initialize
    rename coordinates to drop_points
    switch directions from empty array to empty string
    add sort_ideal_path method to sort route from smallest x value to
    largest
    add validate_drop_points and validate_grid methods
    change math behind convert directions method

--- lib/pizza_bot.rb
+++ lib/pizza_bot.rb
@@ -1,65 +1,96 @@
 class PizzaBot
-  # First round of edits
-  attr_reader :width, :height, :max_dimensions, :coordinates, :directions
+  attr_reader :drop_points, :current_location, :directions
 
   def initialize(string)
-    @width = string[0].to_i
-    @height = string[2].to_i
-    @coordinates = string[4..-1].gsub(/[()]/, "").scan(/.{1,5}/)
-    @max_dimensions = {
-                        max_x:  width - 1,
-                        max_y: height - 1
-                      }
-    @directions = []
+    @drop_points = validate_drop_points(string[4..-1])
+    @grid = validate_grid({width: string[0], height: string[2]})
+    @current_location = {x: 0, y: 0}
+    @directions = ''
   end
 
   def calculate_directions
-    current_x = 0
-    current_y = 0
-
-    coordinates.each do |coordinate|
-      formatted_coordinate = coordinate.strip
-      x_coordinate = formatted_coordinate[0].to_i # 1
-      y_coordinate = formatted_coordinate[-1].to_i # 3
-
-      if x_coordinate > max_dimensions[:max_x] ||
-         y_coordinate > max_dimensions[:max_y]
-         raise ArgumentError.new("Coordinates are out of range for given grid")
-      end
-
-      x_difference = x_coordinate - current_x
-      y_difference = y_coordinate - current_y
-
-      filter_directions('x-axis', x_difference, directions)
-      filter_directions('y-axis', y_difference, directions)
-
-      directions << 'D'
-      current_x = x_coordinate
-      current_y = y_coordinate
-    end
-    directions.join('')
+    sort_ideal_path(drop_points).each {|drop_point| direct_to_drop_point(drop_point)}
   end
 
   private
 
-  def filter_directions(type, difference, directions)
-    if type == 'x-axis'
-      if difference < 0
-        add_directions(difference, 'W')
-      elsif difference > 0
-        add_directions(difference, 'E')
-      end
-    elsif type == 'y-axis'
-      if difference < 0
-        add_directions(difference, 'S')
-      elsif difference > 0
-        add_directions(difference, 'N')
-      end
+  def validate_drop_points(drop_points)
+    formatted_drop_points = drop_points.gsub(/[,()\s+]/, "")
+    if only_integers?(formatted_drop_points)
+      format_drop_points(formatted_drop_points)
+    else
+      raise ArgumentError.new("non-integer value detected")
     end
   end
 
-  def add_directions(difference, value)
-    (difference.abs).times { directions << value }
+  def only_integers?(drop_points)
+    drop_points.scan(/\D/).empty?
+  end
+
+  def format_drop_points(drop_points)
+    # format drop points into an array of key/value pairs i.e. [ {x: 1, y: 2}, {x: 2, y: 4} ]
+    drop_points.scan(/../).map do |drop_point|
+      {x: drop_point[0].to_i, y: drop_point[-1].to_i}
+    end
+  end
+
+  def validate_grid(dimensions)
+    # check if dimensions are integers
+    if !(is_integer?(dimensions[:width]) && is_integer?(dimensions[:height]))
+      raise ArgumentError.new("non-integer value detected")
+    # check if dimensions are within the grid's range
+    elsif !(valid_dimensions?(dimensions))
+      raise ArgumentError.new("drop points are out of range for given grid")
+    else
+      dimensions
+    end
+  end
+
+  def valid_dimensions?(dimensions)
+    # calculate max width and max height
+    max_width = dimensions[:width].to_i - 1
+    max_height = dimensions[:height].to_i - 1
+
+    # calculate largest drop point width/height
+    largest_width = largest_dimension(:x)
+    largest_height = largest_dimension(:y)
+
+    # check to see if largest drop points are within current grid boundaries
+    largest_width <= max_width && largest_height <= max_height
+  end
+
+  def largest_dimension(value)
+    result = drop_points.map { |drop_point| drop_point[value] }
+    result.max
+  end
+
+  def is_integer?(input)
+    input.to_i.to_s == input
+  end
+
+  def direct_to_drop_point(drop_point)
+    # figure out X distance and Y distance
+    delta_x = drop_point[:x] - @current_location[:x]
+    delta_y = drop_point[:y] - @current_location[:y]
+
+    # take that distance and convert to a direction / add the direction to @directions
+    @directions += convert_direction(delta_x, delta_y)
+
+    # update @current_location
+    @current_location[:x] = drop_point[:x]
+    @current_location[:y] = drop_point[:y]
+  end
+
+  def convert_direction(delta_x, delta_y)
+    # given a delta x and delta y, spit out the direction in a string
+    x_direction = delta_x < 0 ? "W" : "E"
+    y_direction = delta_y < 0 ? "S" : "N"
+    (x_direction * (delta_x.abs)) + (y_direction * (delta_y.abs)) + "D"
+  end
+
+  def sort_ideal_path(drop_points)
+    # sort route based on horizontal distance from starting point
+    drop_points.sort_by {|drop_point| drop_point[:x]}
   end
 
 end
