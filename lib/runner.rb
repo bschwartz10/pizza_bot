38e1c5fae7b7e0b2bf99e71e33043e5af38ab8d5
bschwartz10 <bschwartz92@gmail.com>
2018-07-23 16:32:54 -0400

    change way to output directions

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,7 +1,8 @@
 require './lib/pizza_bot'
 
-p PizzaBot.new("5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)").calculate_directions
-
+pizza_bot = PizzaBot.new("5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)")
+pizza_bot.calculate_directions
+p pizza_bot.directions
 # STDIN Solution
 # puts "Please enter directions for our PizzaBot using the following format: 5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
 # string = STDIN.gets.chomp
