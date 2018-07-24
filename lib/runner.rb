025e2ca77eab79725bf92af481653dfa1b825373
bschwartz10 <bschwartz92@gmail.com>
2018-07-24 15:17:49 -0400

    format runner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,7 +3,11 @@ require './lib/pizza_bot'
 pizza_bot = PizzaBot.new("5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)")
 pizza_bot.calculate_directions
 p pizza_bot.directions
-# STDIN Solution
+
+# **** STDIN Solution ****
 # puts "Please enter directions for our PizzaBot using the following format: 5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
-# string = STDIN.gets.chomp
-# p PizzaBot.new(string).calculate_directions
+# input_string = STDIN.gets.chomp
+# pizza_bot = PizzaBot.new(input_string)
+# pizza_bot.calculate_directions
+# route = pizza_bot.directions
+# p "The route for those drop points is #{route}"
